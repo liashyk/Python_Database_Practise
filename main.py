b61194dac5eafa1d2fc97ff7fd8edd9f1a004c9c
Illia Liashenko <ilya9may@gmail.com>
2025-07-30 13:09:04 +0300

    replace manual closing connection with 'with' statement

--- main.py
+++ main.py
@@ -12,67 +12,64 @@ students_csv = Path(config["students_csv"])
 
 df = pd.read_excel(students_xlsx)
 
-con = psycopg2.connect(host = config["host"],
+with psycopg2.connect(host = config["host"],
                         database = config["database"],
                         user = config["user"],
-                        password = config["password"])
+                        password = config["password"]) as con:
 
 
 
-#create table in SQL database
 
-cur = con.cursor()
+    #create table in SQL database
 
-cur.execute(f'DROP TABLE IF EXISTS {config["table_name"]}')
+    with con.cursor() as cur:
 
-cur.execute(f"""CREATE TABLE {config["table_name"]}
-                (id serial PRIMARY KEY,
-                "first name" varchar(40),
-                "second name" varchar(40),
-                "age" int,
-                "average mark" real,
-                "gender" varchar(40),
-                "phone_number" varchar(25))""")
+        cur.execute(f'DROP TABLE IF EXISTS {config["table_name"]}')
 
-
-
-# Remove rows where 'average mark' is missing.
-new_df = df.dropna(subset=['average mark']).copy()
+        cur.execute(f"""CREATE TABLE {config["table_name"]}
+                        (id serial PRIMARY KEY,
+                        "first name" varchar(40),
+                        "second name" varchar(40),
+                        "age" int,
+                        "average mark" real,
+                        "gender" varchar(40),
+                        "phone_number" varchar(25))""")
 
 
-# Split 'student name' into two new columns
-new_df[['first name', 'second name']] = new_df['student name'].str.split(n=1, expand=True)
 
-# Drop the original 'student name' column
-new_df.drop(columns='student name', inplace=True)
-
-#write students in csv format
-new_df.to_csv(students_csv, index=False)
-
-#Insert data into the 'students' table in your DB.
-with open(students_csv, 'r') as f:
-    next(f)
-    cur.copy_from(f, config["table_name"], sep=',', columns=(
-        "age", "average mark", "gender", "phone_number", "first name", "second name"
-    ))
-
-con.commit()
+        # Remove rows where 'average mark' is missing.
+        new_df = df.dropna(subset=['average mark']).copy()
 
 
-#Count the number of students
-cur.execute(f"""
-                SELECT "gender", COUNT(*) 
-                FROM "students" 
-                WHERE "average mark" > 5
-                GROUP BY "gender"
-            """)
+        # Split 'student name' into two new columns
+        new_df[['first name', 'second name']] = new_df['student name'].str.split(n=1, expand=True)
 
-sql_result = cur.fetchall()
+        # Drop the original 'student name' column
+        new_df.drop(columns='student name', inplace=True)
 
-gender_count = pd.DataFrame(sql_result, columns=["gender", "count"])
+        #write students in csv format
+        new_df.to_csv(students_csv, index=False)
 
-print(gender_count)
+        #Insert data into the 'students' table in your DB.
+        with open(students_csv, 'r') as f:
+            next(f)
+            cur.copy_from(f, config["table_name"], sep=',', columns=(
+                "age", "average mark", "gender", "phone_number", "first name", "second name"
+            ))
 
-#close connection to DB
-cur.close()
-con.close()
+        con.commit()
+
+
+        #Count the number of students
+        cur.execute(f"""
+                        SELECT "gender", COUNT(*) 
+                        FROM "students" 
+                        WHERE "average mark" > 5
+                        GROUP BY "gender"
+                    """)
+
+        sql_result = cur.fetchall()
+
+        gender_count = pd.DataFrame(sql_result, columns=["gender", "count"])
+
+        print(gender_count)
